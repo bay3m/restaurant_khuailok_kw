1e345278512f6b36dd56a4ca36df7c1fff0ed29c
bay3m <hansonsiwenbrian@gmail.com>
2024-08-23 15:54:20 +0700

    add button can remove the list

--- src/components/MenuList.jsx
+++ src/components/MenuList.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const MenuList = () => {
     const [menu, setMenu] = useState({});
+    const [removingItem, setRemovingItem] = useState(null);
 
     useEffect(() => {
         const fetchMenu = async () => {
@@ -27,6 +28,19 @@ const MenuList = () => {
         fetchMenu(); 
     }, []); 
 
+    const handleRemoveItem = (category, itemIndex) => {
+        setRemovingItem({ category, itemIndex });
+        setTimeout(() => {
+            setMenu((prevMenu) => {
+                const newMenu = { ...prevMenu };
+                newMenu[category].splice(itemIndex, 1);
+                console.log(newMenu[category])
+                return newMenu; 
+            });
+            setRemovingItem(null);
+        }, 2000);
+    };
+
     return (
         <ul className="menu_list_wrapper">
             {Object.keys(menu).map((category, index) => (
@@ -36,10 +50,12 @@ const MenuList = () => {
                     <div className="menu_item_wrapper">
                         <ul>
                             {menu[category].map((item, itemIndex) => (
-                                <li key={itemIndex} className="menu_item">
+                                <li key={itemIndex} 
+                                    className={`menu_item ${removingItem && removingItem.category === category && removingItem.itemIndex === itemIndex ? 'removing' : ''}`}>
+                                    
                                     {item}
                                     
-                                    <span className="material-symbols-outlined">
+                                    <span onClick={() => handleRemoveItem(category, itemIndex)} className="material-symbols-outlined" id="add_menu_btn">
                                         add
                                     </span>
                                 </li>
@@ -50,6 +66,7 @@ const MenuList = () => {
             ))}
         </ul>
     );
+
 };
 
 export default MenuList;
